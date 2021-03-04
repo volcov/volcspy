571c82706000b1800eef0bd21bf655664aa163df
volcov <volcov.bruno@gmail.com>
2021-03-03 23:50:29 -0300

    assync reviews requests

--- lib/volcspy/scraper.ex
+++ lib/volcspy/scraper.ex
@@ -1,11 +1,20 @@
 defmodule Volcspy.Scraper do
   @site "https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+  @page_range [1, 2, 3, "error", 5]
 
   def get_reviews do
-    get_reviews_per_page(@site, 2)
+    Stream.map(@page_range, fn page -> Task.async(fn -> seek_and_filter(page) end) end)
+    |> Stream.map(fn task -> Task.await(task, 14500) end)
+    |> Stream.concat()
+    |> Enum.to_list()
   end
 
-  defp get_reviews_per_page(base_url, page) do
+  defp seek_and_filter(page) do
+    {:ok, html} = get_reviews_html_per_page(@site, page)
+    filter_review_entries(html)
+  end
+
+  defp get_reviews_html_per_page(base_url, page) do
     url = base_url <> "page#{page}"
 
     case HTTPoison.get(url) do
@@ -18,5 +27,10 @@ defmodule Volcspy.Scraper do
       {:error, %HTTPoison.Error{reason: reason}} ->
         {:error, reason}
     end
+  end
+
+  defp filter_review_entries(review_html_page) do
+    {:ok, document} = Floki.parse_document(review_html_page)
+    Floki.find(document, ".review-entry")
   end
 end
