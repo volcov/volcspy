16c2a87f1972763b974e6dd689acc13fcbf45bee
volcov <volcov.bruno@gmail.com>
2021-03-03 22:55:56 -0300

    add get_reviews function on Scraper

--- lib/volcspy/scraper.ex
+++ lib/volcspy/scraper.ex
@@ -1,2 +1,16 @@
 defmodule Volcspy.Scraper do
+  @site "https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
+
+  def get_reviews(_url, _page) do
+    case HTTPoison.get(@site) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        {:error, :page_not_found}
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
+  end
 end
