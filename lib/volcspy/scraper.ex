36101f8d980c9ce359da22d2fcb57035e6dd9d34
volcov <volcov.bruno@gmail.com>
2021-03-03 23:07:52 -0300

    add get_reviews_per_page function

--- lib/volcspy/scraper.ex
+++ lib/volcspy/scraper.ex
@@ -1,8 +1,14 @@
 defmodule Volcspy.Scraper do
   @site "https://www.dealerrater.com/dealer/McKaig-Chevrolet-Buick-A-Dealer-For-The-People-dealer-reviews-23685/"
 
-  def get_reviews(_url, _page) do
-    case HTTPoison.get(@site) do
+  def get_reviews do
+    get_reviews_per_page(@site, 2)
+  end
+
+  defp get_reviews_per_page(base_url, page) do
+    url = base_url <> "page#{page}"
+
+    case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, body}
 
