5b2c7b70535bcbfa3a7585396233ad3035d541aa
volcov <volcov.bruno@gmail.com>
2021-03-05 21:03:51 -0300

    add some parsers

--- lib/volcspy.ex
+++ lib/volcspy.ex
@@ -4,6 +4,8 @@ defmodule Volcspy do
 
   def scan() do
     reviews = Scraper.get_reviews_html()
-    ReviewParser.get_date(List.first(reviews))
+    # ReviewParser.get_date(List.first(reviews))
+    # ReviewParser.get_title(List.first(reviews))
+    ReviewParser.get_deal_rating(List.first(reviews))
   end
 end
--- lib/volcspy/review_parser.ex
+++ lib/volcspy/review_parser.ex
@@ -1,6 +1,22 @@
 defmodule Volcspy.ReviewParser do
   def get_date(review) do
-    [{_tag_name, _attributes, children_nodes}] = Floki.find(review, ".review-date")
-    children_nodes
+    [{_tag_name, _attributes, [date, _type]}] = Floki.find(review, ".review-date")
+    Floki.text(date)
+  end
+
+  def get_title(review) do
+    review
+    |> Floki.find("h3")
+    |> String.replace("\"", "")
+  end
+
+  def get_deal_rating(review) do
+    review
+    |> Floki.find(".rating-static:first-child")
+    |> Floki.attribute("class")
+    |> List.first()
+    |> String.split()
+    |> Enum.filter(fn attribute -> String.match?(attribute, ~r/rating-\d/) end)
+    |> List.first()
   end
 end
