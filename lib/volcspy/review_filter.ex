3276758efdc37862386f663b6908b6b0c9962042
volcov <volcov.bruno@gmail.com>
2021-03-08 18:27:10 -0300

    add criteria

--- lib/volcspy/review_filter.ex
+++ lib/volcspy/review_filter.ex
@@ -1,6 +1,8 @@
 defmodule Volcspy.ReviewFilter do
   def filter(review_list) do
-    count_employees_reviews(review_list)
+    review_list
+    |> count_employees_reviews()
+    |> three_most_appear()
   end
 
   defp count_employees_reviews(review_list) do
@@ -8,5 +10,11 @@ defmodule Volcspy.ReviewFilter do
     |> Stream.flat_map(& &1.employees)
     |> Stream.map(& &1.name)
     |> Enum.frequencies()
+  end
+
+  defp three_most_appear(review_frequencies) do
+    review_frequencies
+    |> Enum.sort(fn {_name_one, value_one}, {_name_two, value_two} -> value_two <= value_one end)
+    |> Enum.slice(0..2)
   end
 end
