3e29791c4e38d0b22fbd29f0bf7ed4154a86acf1
volcov <volcov.bruno@gmail.com>
2021-03-08 22:14:27 -0300

    review have a reference

--- lib/volcspy/review.ex
+++ lib/volcspy/review.ex
@@ -1,9 +1,10 @@
 defmodule Volcspy.Review do
-  defstruct ~w[date deal_rating title user body review_ratings employees]a
+  defstruct ~w[reference date deal_rating title user body review_ratings employees]a
 
   def new(review, review_ratings, employees)
       when is_map(review) and is_map(review_ratings) and is_list(employees) do
     %__MODULE__{
+      reference: make_ref(),
       date: review.date,
       deal_rating: review.deal_rating,
       title: review.title,
