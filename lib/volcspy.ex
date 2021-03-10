2184b418d14397280a7acb33753a6d0e7fd4e486
volcov <volcov.bruno@gmail.com>
2021-03-09 22:08:09 -0300

    reviews mapped on review module

--- lib/volcspy.ex
+++ lib/volcspy.ex
@@ -1,32 +1,11 @@
 defmodule Volcspy do
   alias Volcspy.Review
   alias Volcspy.ReviewFilter
-  alias Volcspy.ReviewParser
   alias Volcspy.Scraper
 
   def scan() do
     Scraper.get_reviews_html()
-    |> Enum.map(fn review_html -> build_review(review_html) end)
-    |> Enum.map(fn [review_map, review_rating, employees] ->
-      Review.new(review_map, review_rating, employees)
-    end)
+    |> Enum.map(fn review_html -> Review.new(review_html) end)
     |> ReviewFilter.filter_suspect(3)
-  end
-
-  defp build_review(review) do
-    [
-      build_review_map(review),
-      ReviewParser.get_review_ratings(review),
-      ReviewParser.get_employees(review)
-    ]
-  end
-
-  defp build_review_map(review) do
-    %{}
-    |> Map.put(:date, ReviewParser.get_date(review))
-    |> Map.put(:title, ReviewParser.get_title(review))
-    |> Map.put(:deal_rating, ReviewParser.get_deal_rating(review))
-    |> Map.put(:user, ReviewParser.get_user(review))
-    |> Map.put(:body, ReviewParser.get_body(review))
   end
 end
--- lib/volcspy/review.ex
+++ lib/volcspy/review.ex
@@ -1,17 +1,37 @@
 defmodule Volcspy.Review do
+  alias Volcspy.ReviewParser
+
   defstruct ~w[reference date deal_rating title user body review_ratings employees]a
 
-  def new(review, review_ratings, employees)
-      when is_map(review) and is_map(review_ratings) and is_list(employees) do
+  def new(review_html) do
+    [review_map, review_ratings, employees] = build_review(review_html)
+
     %__MODULE__{
       reference: make_ref(),
-      date: review.date,
-      deal_rating: review.deal_rating,
-      title: review.title,
-      user: review.user,
-      body: review.body,
+      date: review_map.date,
+      deal_rating: review_map.deal_rating,
+      title: review_map.title,
+      user: review_map.user,
+      body: review_map.body,
       review_ratings: review_ratings,
       employees: employees
     }
+  end
+
+  defp build_review(review_html) do
+    [
+      build_review_map(review_html),
+      ReviewParser.get_review_ratings(review_html),
+      ReviewParser.get_employees(review_html)
+    ]
+  end
+
+  defp build_review_map(review) do
+    %{}
+    |> Map.put(:date, ReviewParser.get_date(review))
+    |> Map.put(:title, ReviewParser.get_title(review))
+    |> Map.put(:deal_rating, ReviewParser.get_deal_rating(review))
+    |> Map.put(:user, ReviewParser.get_user(review))
+    |> Map.put(:body, ReviewParser.get_body(review))
   end
 end
