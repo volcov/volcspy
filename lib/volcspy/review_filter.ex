b5efe2da67bfe9a0447ff0dea25f180041d285af
volcov <volcov.bruno@gmail.com>
2021-03-12 03:27:04 -0300

    do not need stream here

--- lib/volcspy/review_filter.ex
+++ lib/volcspy/review_filter.ex
@@ -24,8 +24,8 @@ defmodule Volcspy.ReviewFilter do
 
   defp count_employees_reviews(review_list) do
     review_list
-    |> Stream.flat_map(& &1.employees)
-    |> Stream.map(& &1.name)
+    |> Enum.flat_map(& &1.employees)
+    |> Enum.map(& &1.name)
     |> Enum.frequencies()
   end
 
